9c0ed74581f6a0ec993dfddc27ddb58902b51f99
FyperTheViper <b.w.byars@gmail.com>
2018-10-27 19:55:19 -0700

    finished js and some style

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,36 +1,47 @@
 $(document).ready (function () {
 
-    let buttonList = ["Happy", "Laughing", "Fun"];
+    let buttonList = ["LOL", "Dank", "Memes", "Pancakes", "Cats LOL", "Fails"];
     let l = console.log
+
+    buttonPopulator();
 
     $(document).on("click", ".buttonClass", function() {
         let query = $(this).data("name");
         l(query)
-        let gifURL = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=cDJCSVzD3NJUPGzA3ZXqgrUr5aNtZn2w&limit=9";
+        let gifURL = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=cDJCSVzD3NJUPGzA3ZXqgrUr5aNtZn2w&limit=12";
         $.ajax({
             url: gifURL,
             method: "GET"
         })
         .done(function(response){
             l(response);
+            $("#gifSpace").empty();
             let results = response.data;
             for (i = 0; i < results.length; i++) {
-                let gifResult = $("<div class='newGifs'>")
-                let gif = $("<img>");
-                let rating = results[i].rating;
-                let ratingTag = $("<p>").text("Rating: " + rating);
-                let animate = gif.attr("src", results[i].images.fixed_height.url);
-                let still = gif.attr("src", results[i].images.fixed_height_still.url);
-                
-                gif.attr("src", still);
-                gif.attr("data-still", still);
-                gif.attr("data-animate", animate);
-                gif.attr("data-state", still);
-                gif.addClass("submitImage");
-                gifResult.append(ratingTag);
+                let gifResult = $("<div class='newGifs'>");
+                let p = $('<p>').text("Rating: " + results[i].rating);
+                let gif = $('<img>');
+
+                gif.attr("src", results[i].images.fixed_height_still.url);
+                gif.attr("data-state", "still");
+                gif.attr("data-still", results[i].images.fixed_height_still.url);
+                gif.attr("data-animate", results[i].images.fixed_height.url);
+                gif.addClass("gif")
+                gifResult.append(p);
                 gifResult.append(gif);
                 $("#gifSpace").append(gifResult);
             }
+            $(".gif").on("click", function() {
+                let state = $(this).attr("data-state");
+        
+                if (state === "still") {
+                  $(this).attr("src", $(this).attr("data-animate"));
+                  $(this).attr("data-state", "animate");
+                } else {
+                  $(this).attr("src", $(this).attr("data-still"));
+                  $(this).attr("data-state", "still");
+                }
+            });
         })
     })
 
@@ -46,19 +57,10 @@ $(document).ready (function () {
         }
     }
 
-
-    $(".gif").on("click", function() {
-        let state = $(this).attr("data-state");
-
-        if (state === "still") {
-          $(this).attr("src", $(this).attr("data-animate"));
-          $(this).attr("data-state", "animate");
-        } else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-        }
-      });
-
-    buttonPopulator();
-
+    $("#gimme").on("click", function(){
+        let adder = $("#search-input").val().trim();
+        buttonList.push(adder);
+        buttonPopulator();
+        return false;
+    })
 }) 
